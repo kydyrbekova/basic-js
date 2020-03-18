28d4be7e0a9175128e9de33f42f9100b763e3b55
Aigerim <52836936+kydyrbekova@users.noreply.github.com>
2020-03-18 12:41:37 +0600

    Update count-cat.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,15 @@
 module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+  function countCats(backyard) {
+    count = 0
+    for (var i = 0; i < backyard.length; i++) {
+        var innerArrayLength = backyard[i].length;
+        for (var j = 0; j < innerArrayLength; j++) {
+            if (backyard[i][j] == "^^") {
+                count++
+            }
+        }
+    }
+    return count
+}
   // remove line with error and write your code here
 };
