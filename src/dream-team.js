f6d7b7499edd59b67071c79b1ddce6a92bf19584
Aigerim <52836936+kydyrbekova@users.noreply.github.com>
2021-02-08 12:02:40 +0600

    feat: add solution
    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  var teamName=[];
+  var trimmed;
+  if(members == null){return false;}
+  for(var i=0; i<members.length; i++) {
+    if(typeof members[i] === 'string' && members[i].trim() !==null){
+      trimmed = members[i].trim()[0].toUpperCase();
+      teamName.push(trimmed); 
+    }
+  }
+    
+  
+  if(teamName.length>0){
+    return teamName.sort().join("");
+  } else {
+    return false;
+  }
 };
