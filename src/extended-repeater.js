c72dea2c385bb825d83ab24a76659c52b8fbb496
Aigerim <52836936+kydyrbekova@users.noreply.github.com>
2021-02-26 14:55:07 +0600

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,24 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result="";
+    
+    if(options.repeatTimes && options.additionRepeatTimes && typeof(options.repeatTimes) === "number" && typeof(options.additionRepeatTimes)==="number") 
+       {
+        for(var i = 0; i < options.repeatTimes; i++){
+          let subresult = str;
+            for(var j = 0; j < options.additionRepeatTimes; j++){
+                subresult = subresult + options.addition;
+                if(j < options.additionRepeatTimes-1){
+                    subresult = subresult + options.additionSeparator;
+                }
+            }
+            result = result + subresult;
+            if ( i < options.repeatTimes-1){
+                result = result + options.separator;
+            }
+        }
+      return result;
+    } else  {
+    return str;
+    }
 };
   
