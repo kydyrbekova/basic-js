c30b1b949bd79794716bda72503dd96c93aa8825
Aigerim <52836936+kydyrbekova@users.noreply.github.com>
2021-03-07 14:59:17 +0600

    fix: fixed typo and add module.exports

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,34 +1,35 @@
+
 const chainMaker = {
   result: [],
   getLength() {
     return this.result.length;
   },
   addLink(value) {
-    if(value===undefined) {value=" ";}
-    this.result.push(value);
-    return this; 
-    
-  },
+    if(value !== undefined) this.result.push(value);
+     else this.result.push('');
+    return this;
+ },
   removeLink(position) {
-    if(typeof(position) !== 'number' || position < 1 || position > this.result.length){
+    if(typeof(position) != 'number' || position < 1 || position > this.result.length){
       this.result = []
       throw new Error("Position is not correct!");
-    } else {
+    } 
       this.result.splice(position-1, 1)
       return this;
-    } 
+    
   },
   reverseChain() {
     this.result.reverse();
     return this;
   },
   finishChain() {
-    let str = this.result.map(el=> {
-      if(el===" ") { return "( )";} 
-      else { return `( ${el} )`;}
-
+    let str = this.result.map( el => {
+      if(el==='') {return "( )"} else { return `( ${el} )`}
     }).join("~~");
-      this.result = [];
-      return str;
+    this.result = [];
+    return str;
+  
   }
 };
+
+module.exports = chainMaker;
