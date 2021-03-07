eb199fa2b844d2bc6cbd07cbfadd2718538dff6c
Aigerim <52836936+kydyrbekova@users.noreply.github.com>
2021-03-07 14:27:33 +0600

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,74 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(type) {
+        this.type = type;
     }
-
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    
+    encrypt(message, key) {
+        if(message == undefined || key == undefined || message ===null || key === null){
+            throw 'Message and key are mandatory!';
+        }
+        message = message.toUpperCase();
+        key = key.repeat(Math.ceil(message.length/key.length));
+        key = key.toUpperCase();
+        let alphabet=[];
+        for(let i=65; i < 91; i++){
+            alphabet.push(String.fromCharCode(i));
+        }
+     
+        let result =[];
+        let key_index;
+        let message_index;
+        let  alphabet_index;
+        let k=0;
+        for(let j=0; j < message.length; j++) {
+            if(alphabet.includes(message[j])) {
+                 key_index = alphabet.indexOf(key[k]);
+                 k++;
+                 message_index = alphabet.indexOf(message[j]);
+                 alphabet_index = (message_index+key_index < 26 ? message_index+key_index : ((message_index+key_index)-26));
+                 result.push(alphabet[alphabet_index]);
+            }     
+             else {
+                result.push(message[j]);
+            }
+        }
+        if(this.type === false){
+            return result.reverse().join('');
+        }
+        return result.join('');
+    }
+    decrypt(message, key) {
+        if(message === undefined || key === undefined || message ===null || key === null){
+            throw 'Message and key are mandatory!';
+        }
+        message = message.toUpperCase();
+        key = key.repeat(Math.ceil(message.length/key.length));
+        key = key.toUpperCase();
+        let alphabet=[];
+        for(let i=65; i < 91; i++){
+            alphabet.push(String.fromCharCode(i));
+        }
+        let result =[];
+        let key_index;
+        let message_index;
+        let  alphabet_index;
+        let k=0;
+        for(let j=0; j < message.length; j++) {
+            if(alphabet.includes(message[j])) {
+                 key_index = alphabet.indexOf(key[k]);
+                 k++;
+                 message_index = alphabet.indexOf(message[j]);
+                 alphabet_index = (message_index-key_index >=0 ? message_index-key_index : (message_index-key_index+26));
+                 result.push(alphabet[alphabet_index]);
+            }     
+             else {
+                result.push(message[j]);
+            }
+        }
+        if(this.type === false){
+            return result.reverse().join('');
+        }
+        return result.join('');
     }
 }
 
